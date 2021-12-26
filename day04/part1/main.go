80b39e37cb51560c8063d531d021833d03f0eba0
Anton Kosov <AntonRKosov@gmail.com>
2021-12-26 17:40:57 -0600

    day04

---
+++ day04/part1/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strings"
+
+	"github.com/AntonKosov/advent-of-code-2015/aoc"
+)
+
+func main() {
+	data := read()
+	r := process(data)
+	fmt.Printf("Answer: %v\n", r)
+}
+
+func read() string {
+	return aoc.ReadAllInput()[0]
+}
+
+func process(data string) int {
+	for i := 0; ; i++ {
+		p := []byte(fmt.Sprintf("%v%v", data, i))
+		h := md5.Sum(p)
+		s := fmt.Sprintf("%x", h)
+		if strings.HasPrefix(s, "00000") {
+			return i
+		}
+	}
+}
